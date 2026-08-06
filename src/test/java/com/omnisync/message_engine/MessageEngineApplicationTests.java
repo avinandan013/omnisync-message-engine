2ce2fede3bfa58a4c4cad5fb2d6a06036c1f86e1
avinandan013 <avinandan.gayen.13@gmail.com>
2026-08-07 02:22:05 +0530

    Initial commit

---
+++ src/test/java/com/omnisync/message_engine/MessageEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omnisync.message_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
