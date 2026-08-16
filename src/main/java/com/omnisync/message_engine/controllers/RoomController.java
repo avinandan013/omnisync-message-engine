249f60fd5f2f485ab1369fabb661d71a4a890828
avinandan013 <avinandan.gayen.13@gmail.com>
2026-08-16 22:03:44 +0530

    Removing the unwanted changes

--- src/main/java/com/omnisync/message_engine/controllers/RoomController.java
+++ src/main/java/com/omnisync/message_engine/controllers/RoomController.java
@@ -3,16 +3,10 @@ package com.omnisync.message_engine.controllers;
 import com.omnisync.message_engine.Service.CreateRoom;
 import com.omnisync.message_engine.Service.GetMessages;
 import com.omnisync.message_engine.Service.JoinRoom;
-import com.omnisync.message_engine.entity.Message;
-import com.omnisync.message_engine.entity.Room;
-import com.omnisync.message_engine.repositories.RoomRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,23 +14,12 @@ import java.util.List;
 @CrossOrigin("*")
 public class RoomController {
 
-    private RoomRepository roomRepository;
     @Autowired
     private CreateRoom createRoom;
     @Autowired
     private JoinRoom joinRoom;
     @Autowired
     private GetMessages getMessages;
-
-    public RoomController(RoomRepository roomRepository) {
-        this.roomRepository = roomRepository;
-    }
-
-    public RoomController(CreateRoom createRoom) { this.createRoom = createRoom;}
-
-    public RoomController(JoinRoom joinRoom) {this.joinRoom = joinRoom;}
-
-    public RoomController(GetMessages getMessages) {this.getMessages = getMessages;}
 
     //? Create Room
     @PostMapping
